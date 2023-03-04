b0b0402a6e1cc648398284d6c0b27393e17c808a
Ann <krupina.aa@phystech.edu>
2023-03-04 22:49:46 +0300

    Added expense_presenter.py, main logic

---
+++ bookkeeper/presenter/expense_presenter.py
@@ -0,0 +1,27 @@
+from bookkeeper.models.expense import Expense
+
+class ExpensePresenter:
+
+    def __init__(self, model, view, cat_repo, exp_repo):
+        self.model = model
+        self.view = view
+        self.exp_repo = exp_repo
+        self.exp_data = self.exp_repo.get_all()
+        self.cat_data = cat_repo.get_all()
+        self.view.on_expense_add_button_clicked(self.handle_expense_add_button_clicked)
+
+    def update_expense_data(self):
+        self.exp_data = self.exp_repo.get_all()
+        self.view.set_expense_table(self.exp_data)
+
+    def show(self):
+        self.view.show()
+        self.update_expense_data()
+        self.view.set_category_dropdown(self.cat_data)
+
+    def handle_expense_add_button_clicked(self):
+        cat_pk = self.view.get_selected_cat()
+        amount = self.view.get_amount()
+        exp = Expense(float(amount), cat_pk)
+        self.exp_repo.add(exp)
+        self.update_expense_data()
