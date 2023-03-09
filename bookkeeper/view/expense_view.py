26e813ce34110844193c81d93d72035dcf234d8a
Ann <krupina.aa@phystech.edu>
2023-03-09 20:17:10 +0300

    Added budget view realisation and budget functions

--- bookkeeper/view/expense_view.py
+++ bookkeeper/view/expense_view.py
@@ -1,18 +1,18 @@
 from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QGridLayout, QComboBox, QLineEdit, QPushButton
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import QAbstractTableModel, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableView, QTableWidgetItem
 
 
-class TableModel(QtCore.QAbstractTableModel):
+class TableModel(QAbstractTableModel):
     def __init__(self, data):
         super(TableModel, self).__init__()
         self._data = data
 
-    def data(self, index, role):
-        if role == QtCore.Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
+    def data(self, index, role=Qt.DisplayRole) -> str:
+        if index.isValid():
+            if role == Qt.DisplayRole or role == Qt.EditRole:
+                return str(self._data[index.row()][index.column()])
 
     def rowCount(self, index):
         # The length of the outer list.
@@ -23,8 +23,17 @@ class TableModel(QtCore.QAbstractTableModel):
         # the length (only works if all rows are an equal length)
         return len(self._data[0])
 
+    def setData(self, index, value, role) -> bool:
+        if role == Qt.EditRole:
+            self._data[index.row()][index.column()] = value
+            return True
+        return False
 
-class MainWindow(QtWidgets.QMainWindow):
+    def flags(self, index):
+        return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
+
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -40,7 +49,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.layout.addWidget(self.expenses_grid)
 
         self.layout.addWidget(QLabel('Бюджет'))
-        self.layout.addWidget(QLabel('<TODO: таблица бюджета>\n\n\n\n\n\n\n\n'))
+        #self.budget_grid = QtWidgets.QTableView()
+        #self.budget_grid.resizeColumnsToContents()
+        #self.layout.addWidget(self.budget_grid)
+        self.budget_table = QtWidgets.QTableWidget(3, 3)
+        self.budget_table.setColumnCount(3)
+        self.budget_table.setRowCount(3)
+        self.budget_table.setHorizontalHeaderLabels(
+            " :Сумма :Бюджет".split(':'))
+        header_2 = self.budget_table.horizontalHeader()
+        header_2.setSectionResizeMode(
+            0, QtWidgets.QHeaderView.ResizeToContents)
+        header_2.setSectionResizeMode(
+            1, QtWidgets.QHeaderView.ResizeToContents)
+        header_2.setSectionResizeMode(
+            2, QtWidgets.QHeaderView.Stretch)
+        # budget_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.budget_table.verticalHeader().hide()
+        self.budget_table.setItem(0, 0, QTableWidgetItem('День'))
+        self.budget_table.setItem(1, 0, QTableWidgetItem('Неделя'))
+        self.budget_table.setItem(2, 0, QTableWidgetItem('Месяц'))
+        self.layout.addWidget(self.budget_table)
+
 
         self.bottom_controls = QGridLayout()
 
@@ -74,6 +104,29 @@ class MainWindow(QtWidgets.QMainWindow):
     def set_expense_table(self, data):
         self.item_model = TableModel(data)
         self.expenses_grid.setModel(self.item_model)
+
+    def set_budget_table(self, data):
+        budget_month = 30000
+        self.budget_table.setItem(2, 2, QTableWidgetItem(str(budget_month)))
+        self.budget_table.setItem(1, 2, QTableWidgetItem(str(budget_month/4)))
+        self.budget_table.setItem(0, 2, QTableWidgetItem(str(budget_month/30)))
+        sum_today = 0
+        for item in data:
+            sum_today += item[0]
+        self.budget_table.setItem(0, 1, QTableWidgetItem(str(sum_today)))
+
+    def set_budget_table_week_budget(self, data):
+        sum_week = 0
+        for item in data:
+            sum_week += item[0]
+        self.budget_table.setItem(1, 1, QTableWidgetItem(str(sum_week)))
+
+    def set_budget_table_month_budget(self, data):
+        sum_month = 0
+        for item in data:
+            sum_month += item[0]
+        self.budget_table.setItem(2, 1, QTableWidgetItem(str(sum_month)))
+
 
     def set_category_dropdown(self, data):
         for tup in data:
