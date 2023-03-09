edb443f7e53e3a78c527ca492c78f500b639cbfd
Ann <krupina.aa@phystech.edu>
2023-03-09 20:17:57 +0300

    Added budget window to main window

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
+from bookkeeper.presenter.budget_presenter import BudgetPresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
 from bookkeeper.repository.sqlite_repository import SQLiteRepository
@@ -17,6 +18,8 @@ if __name__ == '__main__':
     cat_repo = SQLiteRepository[Category](DB_NAME, Category)
     exp_repo = SQLiteRepository[Expense](DB_NAME, Expense)
 
-    window = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
-    window.show()
+    window_expense = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
+    window_budget = BudgetPresenter(model, view, exp_repo)
+    window_expense.show()
+    window_budget.show()
     app.exec_()
