1e6b92bf2ccc1c91341b71b4c7ae58db35b977f0
Ann <krupina.aa@phystech.edu>
2023-03-13 23:01:48 +0300

    Added documentation, budget functions are with comments, because expense_table doesn't work properly, however in the database everything's ok

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,3 +1,9 @@
+"""
+Модуль содержит код, отвечающий за
+запуск приложения.
+"""
+
+import sys
 from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
@@ -5,7 +11,6 @@ from bookkeeper.presenter.budget_presenter import BudgetPresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
 from bookkeeper.repository.sqlite_repository import SQLiteRepository
-import sys
 
 DB_NAME = 'test.db'
 
@@ -13,13 +18,13 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     view = MainWindow()
-    model = None  # TODO: здесь должна быть модель
+    model = None
 
     cat_repo = SQLiteRepository[Category](DB_NAME, Category)
     exp_repo = SQLiteRepository[Expense](DB_NAME, Expense)
 
-    window_expense = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
-    window_budget = BudgetPresenter(model, view, exp_repo)
+    window_expense = ExpensePresenter(model, view, cat_repo, exp_repo)
+    #window_budget = BudgetPresenter(model, view, exp_repo)
     window_expense.show()
-    window_budget.show()
+    #window_budget.show()
     app.exec_()
